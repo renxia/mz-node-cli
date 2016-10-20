536b366a2fab758173378cfedc8c53c7a328126c
李志文 <lizhiwen@meizu.com>
2016-10-20 20:31:54 +0800

    update cli

--- bin/mznode.js
+++ bin/mznode.js
@@ -23,12 +23,15 @@ function initFis(env) {
     } else {
         fis = require(env.modulePath);
     }
+
+    //commands help 列表
+    fis.set('modules.commands', fis.get('modules.commands').concat(['start']));
+
     // 配置插件查找路径，优先查找本地项目里面的 node_modules
     // 然后才是全局环境下面安装的 fis3 目录里面的 node_modules
     fis.require.paths.unshift(path.join(env.cwd, '../../node_modules'));
     fis.require.paths.unshift(path.join(env.cwd, 'node_modules'));
     fis.require.paths.push(path.join(path.dirname(__dirname), 'node_modules'));
-    // console.log(env);
     fis.cli.run(argv, env);
 }
 
--- fis-conf/parse.js
+++ fis-conf/parse.js
@@ -31,8 +31,7 @@ fis.match('*.less', {
 // npm install -g fis-parser-es6-babel
 // npm install -g fis-parser-babel-5.x
 // 解析 es6 和 react 文件
-// fis.match('/resources/{js,modules}/**.{es,es6,js,jsx},/resources/lib/**.{es,es6}', {
-fis.match(/\.es6?$/, {
+fis.match('/resources/**.{es,es6,jsx}', {
     rExt: '.js',
     parser: fis.plugin('babel-5.x', {
         //plugins: ["transform-runtime"],
@@ -45,8 +44,8 @@ fis.match(/\.es6?$/, {
     //parser: fis.plugin('es6-babel')
 });
 
-// js 目录 js 文件也 babel 编译
-fis.match('/resources/{js,modules, lib/vuex}/**.js', {
+// app 目录 js 文件也 babel 编译
+fis.match('/resources/{app,js,modules, lib/vuex}/**.js', {
     rExt: '.js',
     parser: fis.plugin('babel-5.x', {
         stage: 0,
