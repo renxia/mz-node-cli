2463ddc2e1e786d0ceb612d4bfe58cbfffba68ff
李志文 <lizhiwen@meizu.com>
2017-01-11 11:23:58 +0800

    node_modules 内的文件，prod 模式下也需要压缩

--- fis-conf/prod.js
+++ fis-conf/prod.js
@@ -4,37 +4,35 @@
 
 // optimize压缩、加MD5
 fis.media('prod')
-    .match(/resources\/.*\.(js|es|es6|vue)$/i, { //js 压缩
+    //js 压缩
+    .match('/{resources,node_modules}/**.{js,es,vue,es6,ts,tsx,jsx}', {
         optimizer: fis.plugin('uglify-js', {
             compress: {
                 drop_console: true
             }
         })
     })
-    .match(/resources\/.*\.(css|less)$/i, { //css 压缩
+    //css 压缩
+    .match('/{resources,node_modules}/**.{css,less,sass}', {
         optimizer: fis.plugin('clean-css')
     })
     /*.match(/resources\/.*(-|\.)min\.(js|css)$/i, { // min.js/css 不压缩
             optimizer: false
         })*/
-    .match(/resources\/.*\.(jpg|png|gif|bmp)$/i, { //资源文件添加 hash
+    //资源文件添加 hash
+    .match('/{resources,node_modules}/**.{jpg,png,gif,bmp,jpeg}', {
         useHash: true
     }).match('/resources/static/**', { // static 目录不作 md5 处理
         useHash: false,
         optimizer: false,
         isMod: false
-    }).match('/resources/static/require/**', { // require.js 需要压缩 md5
+    }).match('/resources/static/require/**', { // require.js 需要 md5
         useHash: true
-    }).match('/resources/pkg/**', {
+    }).match('/resources/{pkg,app,js}/**.{js,es,vue,es6,ts,tsx,jsx}', {
         useHash: true
     }).match('/resources/{static,lib}/tongji/**', { // 统计代码需要压缩
         useHash: false,
-        isMod: false,
-        optimizer: fis.plugin('uglify-js', {
-            compress: {
-                drop_console: true
-            }
-        })
+        isMod: false
     });
 
 // 发布时，不 release demo
