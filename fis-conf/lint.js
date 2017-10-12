2c810a25b2c1dbe36c423b31c1a2a118a0d7a63a
renxia <lzwy0820@qq.com>
2017-10-12 11:19:17 +0800

    eslint 规则调整

--- fis-conf/lint.js
+++ fis-conf/lint.js
@@ -7,31 +7,38 @@
 // npm i -g eslint-plugin-react
 // npm install -g fis-lint-eslint
 // 参考：http://smocean.github.io/docs/rules/
-fis.match(/resources\/(app|comm|js)\/.*\.(js|es|es6|es|ts|vue)$/i, {
+fis.match(/resources\/(app|js|comm)\/.*\.(js|es|es6|es|ts|vue)$/i, {
     lint: fis.plugin('eslint', {
         //ignored some files
         //ignored : 'static/libs/**.js',
         ignored: [
-            /\.min\.js$/i
-            // ,/resources\/js\/(common)\/.*/i
+            /\.min\.js$/i,
+            'lib/**.js'
         ],
         "parser": "babel-eslint",
+        "parserOptions": {
+            "sourceType": "module",
+            "allowImportExportEverywhere": true,
+            "codeFrame": false
+        },
+        "envs": ["browser", "es6", "node"],
         "env": {
             "browser": true,
             "node": true,
-            "es6": true
+            "es6": true,
+            "jquery": true,
+            "amd": true,
+            "mocha": true
         },
-        "envs": ["browser", "es6", "node"],
         "globals": {
-            "jest": false,
-            "describe": false,
-            "it": false,
-            "expect": false,
-            "_": false,
-            "$": false,
+            "describe": true,
+            "it": true,
+            "expect": true,
+            "_": true,
+            "$": true,
             "__uri": true,
             "__inline": true,
-            "fis": false,
+            "fis": true,
             "__NAPI_SOURCE__": false,
             "createUnitStore": false,
             "StateTypes": false,
@@ -51,27 +58,32 @@ fis.match(/resources\/(app|comm|js)\/.*\.(js|es|es6|es|ts|vue)$/i, {
             "$TextUtils": true,
             "$Url": true,
             "$Xml": true,
-            "Vue": true
+            "Vue": true,
+            "Flyme": true,
+            "State": true,
+            "Mditor":true
         },
         "rules": { //0 关闭，1 警告，2 错误
-            "block-scoped-var": 0, //把 var 语句看作是在块级作用域范围之内
-            "curly": 1, //为所有控制语句指定花括号约定，警告
-            "eol-last": 0, //强制文件最后一行为空行，关闭
-            "eqeqeq": [1, "allow-null"], //要求使用 === 和 !==，但允许 == null
-            "dot-notation": 2, //尽可能的使用点符号
-            "no-console": 0, //不允许存在 console。关闭
-            "no-empty": 1, //空的代码块
-            "no-multi-spaces": 1, //不允许多个空格
-            "no-self-compare": 1, //禁止自身比较
-            "no-shadow": 1, //不允许声明在外层作用域下已声明过的变量
-            "no-undef": 2, //不允许使用未申明变量
-            "no-underscore-dangle": 0, //禁止标识符中有悬空下划线。关闭
+            "block-scoped-var": 0,                                     //把 var 语句看作是在块级作用域范围之内
+            "comma-dangle": 0,                                         // 不对数组或对象末尾逗号做强制要求
+            "curly": 1,                                                //为所有控制语句指定花括号约定，警告
+            "eol-last": 0,                                             //强制文件最后一行为空行，关闭
+            "eqeqeq": [1, "allow-null"],                               //要求使用 === 和 !==，但允许 == null
+            "dot-notation": 2,                                         //尽可能的使用点符号
+            "no-console": 0,                                           //不允许存在 console。关闭
+            "no-empty": 1,                                             //空的代码块
+            "no-multi-spaces": [1, { "ignoreEOLComments": true }],     //不允许多个空格
+            "no-self-compare": 1,                                      //禁止自身比较
+            "no-shadow": 1,                                            //不允许声明在外层作用域下已声明过的变量
+            "no-undef": 2,                                             //不允许使用未申明变量
+            "no-underscore-dangle": 0,                                 //禁止标识符中有悬空下划线。关闭
             "no-unused-expressions": [1, { allowShortCircuit: true }], // 禁止在语句的位置使用表达式，但允许a && a()
             "no-unused-vars": [2, { "vars": "all", "args": "none" }],  //变量定义后未使用
-            "no-use-before-define": 1, //不允许在变量定义之前使用它们
-            "quotes": [0, "single", "avoid-escape"] //使用单引号，除非为了避免转义
+            "no-use-before-define": 2,                                 //不允许在变量定义之前使用它们
+            "quotes": [0, "single", "avoid-escape"]                    //使用单引号，除非为了避免转义
         },
         "extends": "eslint:recommended",
+        // "extends": ["airbnb"], //新项目最好遵从 airbnb 规范
         "ecmaFeatures": {
             "jsx": true,
             "experimentalObjectRestSpread": true
@@ -84,6 +96,30 @@ fis.match(/resources\/(app|comm|js)\/.*\.(js|es|es6|es|ts|vue)$/i, {
         useEslintrc: false // 不使用 .eslintrc 文件配置
     })
 });
+
+/**************************************
+ * csslint 校验 css/less
+ ***************************************/
+// 使用 csslint 校验代码
+// npm i -g csslint
+// npm install -g fis3-lint-csslint
+// 规则参考：https://github.com/CSSLint/csslint/wiki/Rules
+// var csslintConf = {
+//     ignoreFiles: ['*.min.css'],
+//     rules: {
+//         "rules": {
+//           "known-properties": 2,      //未知属性值
+//           "empty-rules": 1,           //空规则
+//           "duplicate-properties": 1, //重复属性
+//           "overqualified": 0,        //允许元素标签的优先级，如 div.test
+//           "adjoining-classes": 0,    //允许 .foo.bar 格式的连写
+//           "important": 0             //允许 !important
+//         }
+//     }
+// };
+// fis.match('/resources/{app,common}/**/*.{css,less}', {
+//     lint: fis.plugin('csslint', csslintConf)
+// });
 
 /**************************************
  * stylelint 校验 css/less/scss
@@ -106,9 +142,7 @@ var stylelintConf = {
         "defaultSeverity": "error", //默认报告为错误。可选值：warning、error
         "extends": ["stylelint-config-standard"], //加载扩展规则。需要 npm install 在项目目录或父级下
         "rules": { //规则配置，会覆盖 extends 中相同的配置项
-            "comment-empty-line-before": ["always", {
-                "ignore": ["stylelint-commands", "between-comments"],
-            }],
+            "comment-empty-line-before": null,
             "indentation": 4,
             "block-no-empty": true,
             "color-no-invalid-hex": true,
@@ -126,16 +160,15 @@ var stylelintConf = {
             "selector-list-comma-newline-after": null,
             "selector-no-id": true,
             "string-quotes": null, // 'single'
-            "no-missing-end-of-source-newline": null,
+            // "no-missing-end-of-source-newline": null,
             "value-no-vendor-prefix": true
         }
     }
 };
-
-fis.match('/resources/{app,comm,js}/**.{css,less,scss}', {
+fis.match('/resources/{app,js}/**.{css,less,scss}', {
     lint: fis.plugin('stylelint_d', stylelintConf)
 });
 
-fis.match('**.min.css', {
+fis.match('/resources/common/**.{css,less,scss}', {
     lint: false
 });
